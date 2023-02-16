cf0d73748861f21bb4dd7784773a2755e59030b7
calmaxochieng <calmaxochieng@yahoo.com>
2023-02-16 14:16:58 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	printf("size of char:%lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("size of float:%lu byte(s)\n", (unsigned long)sizeof(f));
 	printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("size of long long int: %lu byte(0)\n", (unsigned long)sizeof(c));
+	printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	return (0);
 }
