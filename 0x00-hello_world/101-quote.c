975abf9862727b6fc253db2e625b0d2358d164c5
calmaxochieng <calmaxochieng@yahoo.com>
2023-02-16 15:05:51 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2013-10-19\n,59);
+	return (1);
+}
